04324835eb1604607ee3a67ad48bc58ab446c672
manikandan murugan <manikandan.murugan@basiscloudsolutions.com>
2024-07-17 23:06:24 +0530

    header logo added

---
+++ template/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./img/logo.png";
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <img src={logo} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
